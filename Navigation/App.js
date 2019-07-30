bbdb0e63c310cf4b1eed2b1585c416c1afc37575
brendan richards <brendan.richards@btinternet.com>
2019-07-30 08:59:22 +0100

    navigation and logo

--- Navigation/App.js
+++ Navigation/App.js
@@ -1,49 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import { View, Text, Button, Image} from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class LogoTitle extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Image 
+      source={require('./images/world.png')}
+      style={{ width: 50, height: 50, alignItems: 'center'}} />
+    )
+  }
+}
+
+class HomeScreen extends React.Component {
+  
+  static navigationOptions = {
+    headerTitle: <LogoTitle />,
+  };
+
+
+  render() {
+    return (
+      
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+class DetailsScreen extends React.Component {
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', 'A nested Details Screen'),
+    };
+  };
+
+  render() {
+
+    
+
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemID', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button 
+        title='Go to Details...again'
+        onPress= {() => this.props.navigation.push('Details', {
+          itemId: Math.floor(Math.random() * 100),
+        })}
+        />
+        <Button
+        title='Update the title'
+        onPress={() => this.props.navigation.setParams({ otherParam: 'Updated!'})}
+        />
+         <Button 
+        title='Go to Home'
+        onPress= {() => this.props.navigation.navigate('Home')}
+        />
+         <Button 
+        title='Go back'
+        onPress= {() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  {
+    initialRouteName: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+const AppContainer = createAppContainer(RootStack);
+ 
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
